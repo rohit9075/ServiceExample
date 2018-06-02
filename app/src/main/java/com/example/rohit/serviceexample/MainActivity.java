e27924f84fa1462ba42d0e7904c0ec9ae8e16387
Rohit Kumar <rohit9075@gmail.com>
2018-06-02 12:36:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rohit/serviceexample/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.rohit.serviceexample;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //button objects
+    private Button buttonStart;
+    private Button buttonStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getting button id reference
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
+
+        //attaching onclicklistener to buttons
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonStart) {
+
+
+            boolean bool = isMyServiceRunning(MyService.class);
+
+            // checking the service is running or not if not ru
+            if ( !bool){
+                //starting service
+                startService(new Intent(this, MyService.class));
+            }
+
+        } else if (view == buttonStop) {
+            //stopping service
+            stopService(new Intent(this, MyService.class));
+        }
+    }
+
+
+    // method to check the service is already running or not
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+
+                // returning the true if service is running
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
